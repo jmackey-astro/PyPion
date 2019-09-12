ce54ff5ac73c987b7016dafc71cce5be19afade1
Sam Green <green@cp.dias.ie>
2019-09-12 16:04:31 +0100

    Bash script now works with 2D and 3D data files.

--- Library/plot_nestedgrid.py
+++ Library/plot_nestedgrid.py
@@ -12,19 +12,22 @@
 # - 12-08-2019 SG: Code now using multiple cores.
 
 from Plotting_Classes import Plotting2d
-from argparse_command import InputValues
-from colormaps import cmaps
 import matplotlib.pyplot as plt
 import astropy.units as u
-import multiprocessing as mp
-from joblib import Parallel, delayed
-import dill
 import time
 
-# Saves the relevant information entered in the command.
-line = InputValues()
-time_dicts = line.time_dicts
+import sys
+import warnings
+import matplotlib.cbook
 
+sys.path.append('.')
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
+
+imag = sys.argv[1]
+
+arr = []
+for i in range(2, len(sys.argv)):
+    arr.append(sys.argv[i])
 
 # ################# This is where you change all the parameters. #######################
 # Expected layout: ["variable", max colour value, min colour value, colormap]
@@ -33,27 +36,34 @@ var2 = ["Temperature", 8.2, 4, "gist_heat", 'log']
 # Variable choices: "Density", "DivB", "MagneticFieldX", "MagneticFieldY", "Pressure", "Temperature", "VelocityX",
 # "VelocityY", "VelocityZ", "glmPSI".
 # #######################################################################################
+t0 = time.time()
+
+# First loop goes through each set of files for each timestep.
+# Function to take all the layer files and combine into one figure
+fig = plt.figure()
+imagefile = "%szeta_%s.png" % (imag, arr[0][len(arr[0]) - 22:len(arr[0]) - 6])
+
+# Define the max extents of the figure grid.
+lim = Plotting2d(arr[0])
+lim_min = lim.xmin().to(u.pc)
+lim_max = lim.xmax().to(u.pc)
+lim.close()
+del lim
+
+# Second loop goes through each file in that set and plots it to a figure.
+for f in arr:
+    level = Plotting2d(f)
+    level.plotsilo_2d(lim_min, lim_max, fig, var1, var2)
+
+    level.close()
+    del level
 
 
-def plotter(files):  # for files in time_dicts:
-    # First loop goes through each set of files for each timestep.
-    # Function to take all the layer files and combine into one figure
-    fig = plt.figure()
-    imagefile = "%s%s_%s.png" % (line.img_path, line.img_file, time_dicts[files][0][len(time_dicts[files][0]) - 22:
-                                                                                    len(time_dicts[files][0]) - 6])
-    # Define the max extents of the figure grid.
-    lim_min = Plotting2d(time_dicts[files][0]).xmin().to(u.pc)
-    lim_max = Plotting2d(time_dicts[files][0]).xmax().to(u.pc)
+# This now saves the figure that has had multiple levels plotted onto it.
+plt.savefig(imagefile, bbox_inches='tight', dpi=300)
+plt.close()
 
-    # Second loop goes through each file in that set and plots it to a figure.
-    for f in time_dicts[files]:
-        Plotting2d(f).plotsilo_2d(lim_min, lim_max, fig, var1, var2)
+del fig, imagefile
 
-    # This now saves the figure that has had multiple levels plotted onto it.
-    plt.savefig(imagefile, bbox_inches='tight', dpi=300)
-    plt.close()
-
-
-# To run the code with multiple processors:
-num_cores = mp.cpu_count()
-Parallel(n_jobs=num_cores)(delayed(plotter)(files) for files in time_dicts)
+t1 = time.time()
+print("Running Time: {0}".format(t1 - t0))
