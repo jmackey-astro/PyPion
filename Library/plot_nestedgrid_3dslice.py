7d0086486d2c29f246992b05de372effd7b5f0b2
Sam Green <green@berial.ap.dias.ie>
2019-10-22 17:30:17 +0100

    Fixed some errors in bash script (see file comments for more info).

--- Library/plot_nestedgrid_3dslice.py
+++ Library/plot_nestedgrid_3dslice.py
@@ -24,7 +24,7 @@ for i in range(2, len(sys.argv)):
     arr.append(sys.argv[i])
 
 
-var1 = ["Density", -21, -27, "viridis", 'log', 'y', 63]
+var1 = ["Density", -21, -27, "viridis", 'log', 'y', 127]
 
 # for files in InputValues().time_dicts:
 t0 = time.time()
@@ -32,7 +32,7 @@ t0 = time.time()
 fig = plt.figure()
 gs = gridspec.GridSpec(2, 2, height_ratios=[1, 0.05], width_ratios=[1, 1])
 gs.update(left=0.05, right=0.95, bottom=0.08, top=0.93, wspace=0.02, hspace=0.03)
-imagefile = "%szeta_%s.png" % (imag, arr[0][len(arr[0]) - 22:len(arr[0]) - 6])
+imagefile = "%sfig%s.png" % (imag, arr[0][len(arr[0]) - 13:len(arr[0]) - 6])
 
 lim = Plotting3d(arr[0])
 lim_min = lim.xmin().to(u.pc)
@@ -54,5 +54,3 @@ del fig, gs, imagefile
 
 t1 = time.time()
 print("Running Time: {0}".format(t1 - t0))
-
-
