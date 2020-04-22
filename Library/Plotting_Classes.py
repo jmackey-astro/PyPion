0faf7cef393772191db8f0b14daa8aca9038d8c2
Sam Green <green@cp.dias.ie>
2020-04-22 18:22:41 +0100

    Updated the function to plot slices of 3D data with the new ReadData.py. Still a work in progress but it works on a basic level

--- Library/Plotting_Classes.py
+++ Library/Plotting_Classes.py
@@ -17,7 +17,7 @@
 # -2019-08-12 SG: Tidied up a few things.
 
 # -------------- Set of libraries needed:
-from ReadData import Read2dSiloData, Read3dSiloData
+from ReadData import ReadData
 
 import matplotlib
 # Using this to stop matplotlib from using a $DISPLAY environment variable.
@@ -28,9 +28,13 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.ticker import MultipleLocator
+import matplotlib.gridspec as gridspec
 
 import numpy as np
 from astropy import units as u
+
+import warnings
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 
 plt.rc('font', **{'size': 12})
 # plt.rc('lines', linewidth=2)
@@ -38,10 +42,9 @@ plt.rc('font', weight='bold')  # <-------------
 
 # -------------- Class to plot the data from 2d Silo files.
 
-
-class Plotting2d(Read2dSiloData):
+class Plotting2d(ReadData):
     def plotsilo_2d(self, lim_min, lim_max, fig, var1, var2, alpha=1):
-        xmin = self.xmin().to(u.pc)  # Set data to units of parsecs.
+        xmin = (self.xmin() * u.cm).to(u.pc)  # Set data to units of parsecs.
         xmax = self.xmax().to(u.pc)
 
         var1 = var1
@@ -112,91 +115,79 @@ class Plotting2d(Read2dSiloData):
         return fig
 
 
-class Plotting3d(Read3dSiloData):
-    def plotsilo_3dslice(self, lim_min, lim_max, fig, gs, var1, alpha=1):
-        xmin = self.xmin().to(u.pc)  # Set data to units of parsecs.
-        xmax = self.xmax().to(u.pc)
+class Plotting(ReadData):
+    def plot3Dslice(self):
 
-        var1 = var1
+        # assign necessary data to arrays.
+        data = self.get_3Darray()['data']
+        lim_max = (self.get_3Darray()['max_extents'] * u.cm).to(u.pc)
+        lim_min = (self.get_3Darray()['min_extents'] * u.cm).to(u.pc)
+        sim_time = self.get_3Darray()['sim_time']
 
-        level_min = self.level_min().to(u.pc)  # Set data to units of parsecs.
-        level_max = self.level_max().to(u.pc)
+        #var1 = var1
 
-        if var1[0] == 'B3D_mag':
-            magx = self.reshaped_parameter3d('MagneticFieldX')
-            magy = self.reshaped_parameter3d('MagneticFieldY')
-            magz = self.reshaped_parameter3d('MagneticFieldZ')
+        fig = plt.figure()
+        gs = gridspec.GridSpec(2, 2, height_ratios=[1, 0.05], width_ratios=[1, 1])
+        gs.update(left=0.05, right=0.95, bottom=0.08, top=0.93, wspace=0.02, hspace=0.03)
 
-            mag = np.sqrt((magx ** 2) + (magy ** 2) + (magz ** 2))
+        for i in range(len(data)):
+            x_slice = data[i][63, :, :]
+            y_slice = data[i][:, 63, :]
 
-            log_dx = np.log10(mag[var1[6], :, :])
-            log_dy = np.log10(mag[:, var1[6], :])
+            log_dx = np.log10(x_slice)
+            log_dy = np.log10(y_slice)
 
-        else:
-            x_sliced_parameter3d = self.reshaped_parameter3d(var1[0])[var1[6], :, :]
-            y_sliced_parameter3d = self.reshaped_parameter3d(var1[0])[:, var1[6], :]
+            # --------------Left Plot----------------------------
 
-            log_dx = np.log10(x_sliced_parameter3d)
-            log_dy = np.log10(y_sliced_parameter3d)
+            ax1 = fig.add_subplot(gs[0, 0])
 
-        '''
-        if var1[4] == 'log':
-            log_dx = np.log10(x_sliced_parameter3d)
-            log_dy = np.log10(y_sliced_parameter3d)
-            log_dz = np.log10(z_sliced_parameter3d)
-        else:
-            log_dx = x_sliced_parameter3d
-            log_dy = y_sliced_parameter3d
-            log_dz = z_sliced_parameter3d
-        '''
-        # --------------Left Plot----------------------------
-        ax1 = fig.add_subplot(gs[0, 0])
-        ax1.set_title('                                                   Time = %5.5f Myr' % self.sim_time().value)
+            ax1.set_title('                                                 Time = %5.5f Myr' % sim_time)
 
-        ax1.set_xlim(lim_min[0].value, lim_max[0].value)
-        ax1.set_ylim(lim_min[1].value, lim_max[1].value)
+            ax1.set_xlim(lim_min[0][0].value, lim_max[0][0].value)
+            ax1.set_ylim(lim_min[0][1].value, lim_max[0][1].value)
 
-        im1 = ax1.imshow(log_dy, interpolation='nearest', cmap=var1[3],
-                         extent=[level_min[0].value, level_max[0].value, level_min[1].value,
-                                 level_max[1].value],
-                         origin='lower', vmax=var1[1], vmin=var1[2], alpha=alpha)
-        # txt1 = ax1.text(0.8, 0.92, r'$log(\rho)$', transform=ax1.transAxes)
-        ax1.set_xlabel('x-axis (pc)')
-        ax1.set_ylabel('z-axis (pc)')
+            im1 = ax1.imshow(log_dy, interpolation='nearest', cmap="viridis",
+                            extent=[lim_min[i][0].value, lim_max[i][0].value, lim_min[i][1].value, lim_max[i][1].value],
+                            origin='lower', vmax=-22, vmin=-27)
+            # txt1 = ax1.text(0.8, 0.92, r'$log(\rho)$', transform=ax1.transAxes)
+            ax1.set_xlabel('x-axis (pc)')
+            ax1.set_ylabel('z-axis (pc)')
 
-        # --------------Right Plot----------------------------
-        ax2 = fig.add_subplot(gs[0, 1])
-        # ax2.set_title('Time = %5.5f Myr' % self.sim_time().value)
+            # --------------Right Plot----------------------------
+            ax2 = fig.add_subplot(gs[0, 1])
+            # ax2.set_title('Time = %5.5f Myr' % self.sim_time().value)
 
-        ax2.set_xlim(lim_min[0].value, lim_max[0].value)
-        ax2.set_ylim(lim_min[2].value, lim_max[2].value)
+            ax2.set_xlim(lim_min[0][0].value, lim_max[0][0].value)
+            ax2.set_ylim(lim_min[0][2].value, lim_max[0][2].value)
 
-        im2 = ax2.imshow(log_dx, interpolation='nearest', cmap=var1[3],
-                         extent=[level_min[0].value, level_max[0].value, level_min[2].value,
-                                 level_max[2].value],
-                         origin='lower', vmax=var1[1], vmin=var1[2], alpha=alpha)
-        # txt2 = ax2.text(0.8, 0.92, r'$log(\rho)$', transform=ax1.transAxes)
-        ax2.set_xlabel('x-axis (pc)')
-        ax2.yaxis.set_label_position("right")
-        ax2.yaxis.tick_right()
-        ax2.set_ylabel('y-axis (pc)')
+            im2 = ax2.imshow(log_dx, interpolation='nearest', cmap="viridis",
+                            extent=[lim_min[i][0].value, lim_max[i][0].value, lim_min[i][2].value, lim_max[i][2].value],
+                            origin='lower', vmax=-22, vmin=-27)
+            # txt2 = ax2.text(0.8, 0.92, r'$log(\rho)$', transform=ax1.transAxes)
+            ax2.set_xlabel('x-axis (pc)')
+            ax2.yaxis.set_label_position("right")
+            ax2.yaxis.tick_right()
+            ax2.set_ylabel('y-axis (pc)')
 
-        cbax = plt.subplot(gs[-1, 0:])
-        cb = Colorbar(ax=cbax, mappable=im1, orientation='horizontal', ticklocation='bottom')
-        # cb.set_label(r'Colorbar !', labelpad=10)
-
-        del xmin
-        del xmax
-        del log_dx
-        del log_dy
-        #del x_sliced_parameter3d
-        #del y_sliced_parameter3d
-        del var1
-        del level_min
-        del level_max
-        del im1
-        del im2
-        del ax1
-        del ax2
+            cbax = plt.subplot(gs[-1, 0:])
+            cb = Colorbar(ax=cbax, mappable=im1, orientation='horizontal', ticklocation='bottom')
+            # cb.set_label(r'Colorbar !', labelpad=10)
 
         return fig
+
+'''
+if var1[0] == 'B3D_mag':
+    magx = self.get_3Darray('MagneticFieldX')
+    magy = self.get_3Darray('MagneticFieldY')
+    magz = self.get_3Darray('MagneticFieldZ')
+
+    mag = np.sqrt((magx ** 2) + (magy ** 2) + (magz ** 2))
+
+    log_dx = np.log10(mag[var1[6], :, :])
+    log_dy = np.log10(mag[:, var1[6], :])
+'''
+
+arr = ('files/zeta_10uG_level00_0000.00100000.silo', 'files/zeta_10uG_level01_0000.00100000.silo', 'files/zeta_10uG_level02_0000.00100000.silo')
+
+a = Plotting(arr, 'Density').plot3Dslice()
+plt.savefig("test.png", bbox_inches='tight', dpi=300)
