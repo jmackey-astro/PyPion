199b7b14fb55c240a73332df751768a5cfb06509
Sam Green <green@cp.dias.ie>
2020-10-22 17:21:43 +0100

    Removed unnecessary comments.

--- Library/PlotData.py
+++ Library/PlotData.py
@@ -28,18 +28,14 @@ for files in time_dicts:
 
 	fig = plt.figure()
 
-        if dimen == "2d" or dimen == "2D":
+     if dimen == "2d" or dimen == "2D":
+          a = Plotting2d(arr).plot2d_1(var1[0], fig, var1)
 
-             # print(dimen)
-             a = Plotting2d(arr).plot2d_1(var1[0], fig, var1)
+     elif dimen == "3d" or dimen == "3D":
+          a = Plotting3d(arr).XZXYslice(var1[0], fig, var1)
 
-        elif dimen == "3d" or dimen == "3D":
-
-             # print(dimen)
-             a = Plotting3d(arr).XZXYslice(var1[0], fig, var1)
-
-        else:
-             print("Please choose a correct dimenion (1d, 2d, or 3d)")
+     else:
+          print("Please choose a correct dimenion (1d, 2d, or 3d)")
 
 	imagefile = "%s%s_%s.png" % (line.img_path, line.img_file, time_dicts[files][0][len(time_dicts[files][0]) - 13:len(time_dicts[files][0]) - 5])
 	plt.savefig(imagefile, bbox_inches='tight', dpi=300)
